03bf017397124179b69feb48f52391d61e684fb6
Neexol <almostroll@yandex.ru>
2023-05-12 16:21:54 +0300

    Add file reading and POS tagging

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,17 @@ import opennlp.tools.postag.POSModel
 import opennlp.tools.postag.POSTaggerME
 import java.io.File
 
+fun getWords(): Array<String> {
+    val text = File("text.txt").readText()
+    return text.split(Regex("\\W+")).filter { it.isNotEmpty() }.toTypedArray()
+}
+
+fun getTags(words: Array<String>): Array<String> {
+    return POSTaggerME(POSModel(File("models/en-pos-maxent.bin").inputStream())).tag(words)
+}
+
 fun main() {
-    val text = "John has a sister named Penny."
-    val tokens = text.split(Regex("\\W+")).filter { it.isNotEmpty() }.toTypedArray()
-    val tags = POSTaggerME(POSModel(File("models/en-pos-maxent.bin").inputStream())).tag(tokens)
-    println(tokens.zip(tags))
+    val words = getWords()
+    val tags = getTags(words)
+    println(words.zip(tags))
 }
