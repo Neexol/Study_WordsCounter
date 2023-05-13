595c8e93bca4304cdda83f1fe4b93dcee7365112
Neexol <almostroll@yandex.ru>
2023-05-13 13:22:26 +0300

    Add coverage report generation in xml and csv formats

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,11 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
 }
 
 kotlin {
